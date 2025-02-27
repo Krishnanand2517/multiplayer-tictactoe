ee82e10e15207811688b25cbad249606528a1108
Krishnanand Yadav <krishnanand2517@gmail.com>
2025-02-27 22:55:35 +0530

    feat: Setup react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <div>
       <h1 className="text-2xl">Hey there 👋</h1>
+      <Outlet />
     </div>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,28 @@
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
 import App from "./App.tsx";
 import { SocketProvider } from "./context/ContextProvider.tsx";
+import HomeScreen from "./screens/HomeScreen.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomeScreen />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <>
     <SocketProvider>
-      <App />
+      <RouterProvider router={router} />
     </SocketProvider>
   </>
 );
