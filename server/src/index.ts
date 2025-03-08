8eba9a6e382320a718ab36c0dec1b6dc21cb4255
Krishnanand Yadav <krishnanand2517@gmail.com>
2025-03-08 18:00:31 +0530

    feat: Write the room joining logic for the socket server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,10 @@
 import { Server } from "socket.io";
 
+interface RoomJoinData {
+  username: string;
+  roomId: string;
+}
+
 const io = new Server(parseInt(process.env.PORT!) || 8000, {
   cors: {
     origin: "http://localhost:5173",
@@ -8,6 +13,17 @@ const io = new Server(parseInt(process.env.PORT!) || 8000, {
 
 io.on("connection", (socket) => {
   console.log("Socket connected:", socket.id);
+
+  socket.on("room:join", async (data: RoomJoinData) => {
+    const { username, roomId } = data;
+
+    io.to(roomId).emit("user:joined", { username, socketId: socket.id });
+
+    await socket.join(roomId);
+    console.log(`${username} joined the room - ${roomId}`);
+
+    io.to(socket.id).emit("room:join", { roomId });
+  });
 
   socket.on("disconnect", () => {
     console.log("Disconnected:", socket.id);
